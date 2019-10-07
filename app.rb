fcf971863694defe4b4cdbd5cdb38804efa67e9d
Rintaro Kudo <rintaro.kudo@keio.jp>
2019-10-07 04:04:16 +0000

    cloudinary setting done

--- app.rb
+++ app.rb
@@ -6,6 +6,15 @@ require 'sinatra/activerecord'
 require './models'
 
 
+before do
+  Dotenv.load
+  Cloudinary.config do |config|
+    config.cloud_name = ENV['CLOUD_NAME']
+    config.api_key = ENV['CLOUDINARY_API_KEY']
+    config.api_secret = ENV['CLOUDINARY_API_SECRET']
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -15,6 +24,18 @@ get '/new' do
 end
 
 post '/new' do
+  img_url = ''
+  if params[:file]
+    img = params[:file]
+    tempfile = img[:tempfile]
+    upload = Cloudinary::Uploader.upload(tempfile.path)
+    img_url = upload['url']
+  end
+
+  Restaurant.create({
+  body: params[:body],
+  img: img_url
+  })
 
 
   redirect '/new/done'
@@ -23,6 +44,11 @@ end
 get '/new/done' do
   erb :post_complete
 end
+
+get '/back' do
+  erb :index
+end
+
 
 get '/find' do
   erb :select_camp
