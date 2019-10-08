0eeacbe0535aa690fc984c74fe014178646c834b
Rintaro Kudo <rintaro.kudo@keio.jp>
2019-10-08 16:05:41 +0000

    added lon and lat column to restaurants

---
+++ db/migrate/20191008155339_add_lat_and_lon_to_restaurants.rb
@@ -0,0 +1,8 @@
+class AddLatAndLonToRestaurants < ActiveRecord::Migration[5.2]
+  def change
+
+      add_column :restaurants, :lat, :float
+      add_column :restaurants, :lon, :float
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_124309) do
+ActiveRecord::Schema.define(version: 2019_10_08_155339) do
 
   create_table "camps", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_10_07_124309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+    t.float "lat"
+    t.float "lon"
   end
 
 end
