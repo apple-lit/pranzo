813ff290e581bd6c01dc4905f2d42dd9157b484f
Rintaro Kudo <rintaro.kudo@keio.jp>
2019-10-14 08:09:40 +0000

    map update

--- app.rb
+++ app.rb
@@ -77,9 +77,9 @@ end
 post '/find/list' do
   @camp_place = Camp.all.order('id desc')
   @list = Restaurant.where(camp: params[:camp_place])
-  @camp_map = Camp.where(name: params[:camp_place])
-  @camp_lat = Camp.find(lat: params[:lat])
-  @camp_lon = Camp.find(lon: params[:lon])
+  @camp_map = Camp.find_by(name: params[:camp_place])
+  # @camp_lat = Camp.find(lat: params[:lat])
+  # @camp_lon = Camp.find(lon: params[:lon])
   erb :list
 end
 
