837910a551b63c13383c9511eba9c25923f5c102
Rintaro Kudo <rintaro.kudo@keio.jp>
2019-10-14 00:46:01 +0000

    seeds.rb added

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+Camp.create(:name => '慶應義塾大学SFC', :lat => 35.38808, :lon => 139.4272151)
+
+Camp.create(:name => '東京大学浅野キャンパス', :lat => 35.7160948, :lon => 139.7618559)
+
+Camp.create(:name => '早稲田大学', :lat => 35.7053219, :lon => 139.7188097)
+
+Camp.create(:name => '学習院大学', :lat => 35.7184815, :lon => 139.706305)
+
+Camp.create(:name => '横浜国立大学', :lat => 35.4721859, :lon => 139.5922335)
+
+Camp.create(:name => '成蹊大学', :lat => 35.7136282, :lon => 139.5713447)
+
+Camp.create(:name => 'デジタルハリウッド大学', :lat => 35.6986943, :lon => 139.7641302)
+
+Camp.create(:name => '東京都市大学', :lat => 35.6121146, :lon => 139.6265615)
+
+Camp.create(:name => '関西大学', :lat => 34.7063096, :lon => 135.4988937)
+
+Camp.create(:name => '立命館大学', :lat => 35.38808, :lon => 139.4272151)
+
+Camp.create(:name => '名城大学', :lat => 35.38808, :lon => 139.4272151)
+
+Camp.create(:name => '信州大学', :lat => 336.2496713, :lon => 137.9764472)
+
+Camp.create(:name => '北海道大学', :lat => 35.38808, :lon => 139.4272151)
+
+Camp.create(:name => '東北大学', :lat => 35.38808, :lon => 139.4272151)
+
+Camp.create(:name => '九州大学', :lat => 35.38808, :lon => 139.4272151)
